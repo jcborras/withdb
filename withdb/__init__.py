17d1e64a5eef32972dda9332be44b781014de61c
Juan Carlos Borrás <jcborras@gmail.com>
2018-05-31 00:12:04 +0300

    psql proxy works (v0.0.2)

--- withdb/__init__.py
+++ withdb/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8; mode: python; -*-
 
 __author__ = 'Juan Carlos Borrás'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from withdb.mysql import MySQLconnection
 from withdb.psql import PostgreSQLconnection
