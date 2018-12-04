b0cd79b52268bbed48770aa30d5f688a8af52f82
Juan Carlos Borrás <jcborras@gmail.com>
2018-12-04 23:47:57 +0200

    Refactored config file reading into the setUp method

--- test/test_psql.py
+++ test/test_psql.py
@@ -14,38 +14,46 @@ import withdb
 class TestDrive(TestCase):
     CFG_FILE = expanduser('~/.withdb_test_psql.cfg')
 
+    def setUp(self):
+        with open(self.CFG_FILE, 'r') as f:
+            self.cfg = load(f)
+
     def test_00_psql(self):
         self.assertIsNotNone(withdb.psql.PostgreSQLconnection('foo'))
 
-    def test_01_open_config_file(self):
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        self.assertIsNotNone(cfg)
-        self.assertEqual(cfg['type'], 'psql')
+    def test_10_open_config_file(self):
+        self.assertIsNotNone(self.cfg)
+        self.assertEqual(self.cfg['type'], 'psql')
 
-    def test_02_factory_psql_1(self):
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        self.assertIsNotNone(withdb.factory(cfg))
-        self.assertIsInstance(withdb.factory(cfg),
+    def test_20_factory_psql_1(self):
+        self.assertIsNotNone(withdb.factory(self.cfg))
+        self.assertIsInstance(withdb.factory(self.cfg),
                               withdb.psql.PostgreSQLconnection)
 
-    def test_03_factory_psql_2(self):
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        self.assertIsInstance(withdb.factory(cfg),
+    def test_30_factory_psql_and_select(self):
+        """Check that a SELECT query runs fine"""
+        self.assertIsInstance(withdb.factory(self.cfg),
                               withdb.psql.PostgreSQLconnection)
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             qry = "SELECT * FROM information_schema.attributes;"
             rows, cols = conn(qry)
         self.assertEqual(len(rows), 0)
         self.assertIn('attribute_name', cols)
         self.assertIn('scope_catalog', cols)
 
-    def test_04_create_table(self):
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        with withdb.factory(cfg) as conn:
+    def test_31_factory_psql_and_select_is_logger(self):
+        """Check that a SELECT query runs fine and logged as DEBUG"""
+        self.assertIsInstance(withdb.factory(self.cfg),
+                              withdb.psql.PostgreSQLconnection)
+        with withdb.factory(self.cfg) as conn:
+            qry = "SELECT * FROM information_schema.attributes;"
+            rows, cols = conn(qry)
+        self.assertEqual(len(rows), 0)
+        self.assertIn('attribute_name', cols)
+        self.assertIn('scope_catalog', cols)
+
+    def test_40_create_table(self):
+        with withdb.factory(self.cfg) as conn:
             create_qry = """
             CREATE TABLE on_timezones (
             entry_id INT NOT NULL PRIMARY KEY,
@@ -54,21 +62,19 @@ class TestDrive(TestCase):
             ts_with_tz timestamp with time zone
             );"""
             conn(create_qry)
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             rows, cols = conn("SELECT COUNT(*) AS row_count FROM on_timezones")
         self.assertEqual(rows[0], (0,))
 
-    def test_05_drop_table(self):
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        with withdb.factory(cfg) as conn:
+    def test_50_drop_table(self):
+        with withdb.factory(self.cfg) as conn:
             conn("DROP TABLE IF EXISTS on_timezones")
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             self.assertRaises(
                 RuntimeError, conn,
                 "SELECT COUNT(*) AS row_count FROM on_timezones")
 
-    def test_06_load_lod(self):
+    def test_60_load_lod(self):
         utc_tz = timezone('UTC')
         hel_tz = timezone('Europe/Helsinki')
         cph_tz = timezone('Europe/Copenhagen')
@@ -94,16 +100,13 @@ class TestDrive(TestCase):
                 'ts_with_tz':  cph_tz.localize(NOW),
             },
         ]
-        print()
         KEYS = ['entry_id', 'tz_at_insert', 'ts_without_tz', 'ts_with_tz']
         for i in lod:
             # print(i)
             self.assertEqual(
                 i['ts_without_tz'], i['ts_with_tz'].astimezone(utc_tz))
 
-        with open(self.CFG_FILE, 'r') as f:
-            cfg = load(f)
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             create_qry = """
             CREATE TABLE on_timezones (
             entry_id INT NOT NULL PRIMARY KEY,
@@ -112,23 +115,22 @@ class TestDrive(TestCase):
             ts_with_tz timestamp with time zone
             );"""
             conn(create_qry)
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             rows, cols = conn("SELECT COUNT(*) AS row_count FROM on_timezones")
         self.assertEqual(rows[0], (0,))
 
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             _ = ['entry_id', 'tz_at_insert', 'ts_without_tz', 'ts_with_tz']
             conn.bulkload_lod(
-                lod, _, 'on_timezones', cfg['tmp_dir'] + '/test_psql_')
-        with withdb.factory(cfg) as conn:
+                lod, _, 'on_timezones', self.cfg['tmp_dir'] + '/test_psql_')
+        with withdb.factory(self.cfg) as conn:
             rows, cols = conn("SELECT * FROM on_timezones")
             self.assertEqual(len(rows), 3)
             _ = [dict(zip(cols, i)) for i in rows]
             for i in _:
-                # print(i)
                 _ = i['ts_with_tz'].astimezone(timezone(i['tz_at_insert']))
                 self.assertEqual(i['ts_without_tz'], _.replace(tzinfo=None))
-        with withdb.factory(cfg) as conn:
+        with withdb.factory(self.cfg) as conn:
             conn("DROP TABLE IF EXISTS on_timezones")
 
 
